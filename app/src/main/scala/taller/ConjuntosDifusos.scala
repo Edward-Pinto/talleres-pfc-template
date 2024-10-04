1151b00bb3b250aa3aa050725602fdac6b8b7af2
F4RGAN2007 <avalo.santiago@correounivalle.edu.co>
2024-10-04 15:50:01 -0500

    Creacion metodos union, interseccion y diferencia

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -8,21 +8,40 @@ class ConjuntosDifusos() {
         s(elem)
     }
 
+    def muchoMayorQue ( a : Int, m: Int ) : ConjDifuso = {
+        def mma( x :Int) : Double = {
+            if ( x <= a ) 0.0
+            else if ( x > a && x <= m) (x-a).toDouble / (m-a).toDouble
+            else 1.0
+        }
+        mma
+    }
+
+
     def grande(d: Int, e: Int): ConjDifuso = {
         require(d >= 1, "d debe ser mayor o igual a 1")
-        0
         (n: Int) => Math.pow(n.toDouble / (n + d), e)
     }
-    def interseccion( cd1 : ConjDifuso , cd2 : ConjDifuso ) : ConjDifuso = {
-    // Implementaci´on de la funci´on interseccion
 
+    
+    def complemento (c : ConjDifuso) : ConjDifuso = {
+        (x: Int) => 1.0 - c(x)
     }
+
+    def union( cd1 : ConjDifuso , cd2 : ConjDifuso ) : ConjDifuso = {
+        (x: Int) => math.max(cd1(x) , cd2(x))
+    }
+
+    def interseccion( cd1 : ConjDifuso , cd2 : ConjDifuso ) : ConjDifuso = {
+        (x: Int) => math.min(cd1(x) , cd2(x))
+    }
+    /*
     def inclusion ( cd1 : ConjDifuso , cd2 : ConjDifuso ) : Boolean = {
-    // Implementaci´on de la funci´on inclusion
 
     }
     def igualdad ( cd1 : ConjDifuso , cd2 : ConjDifuso ) : Boolean = {
-    // Implementaci´on de la funci´on igualdad
+
 
     }
+    */
 }
